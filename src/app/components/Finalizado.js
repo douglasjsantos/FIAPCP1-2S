d0e4a1bbab6ce95836e7732d3980c6b0cb03cc58
Douglas <douglas_j.santos@hotmail.com>
2023-09-04 15:42:02 -0300

    ajustes no layout

---
+++ src/app/components/Finalizado.js
@@ -0,0 +1,8 @@
+export default function Finalizado() {
+    return (
+        <div className="w-300 h-64 bg-blue-500 rounded-lg relative shadow-xl" style={{ backgroundColor: "#0092DD" }}>
+        <h1 className="text-5xl text-white text-center font-extrabold mt-16">Tudo certo <br /> O seu guincho <br /> foi realizado</h1>
+    </div>
+    
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Banner from "./components/Banner"
 import Banner2 from "./components/Banner2"
 import Banner3 from "./components/Banner3"
 import Selecao from "./components/Selecao"
+import Finalizado from "./components/Finalizado"
 export default function Home() {
   return (
     <main>
@@ -15,7 +16,7 @@ export default function Home() {
       <br></br>
       <Banner3 />
       <Selecao/>
-      
+      <Finalizado/>
       {/* <button className="bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">Selecionar imagem</button>
       <button className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded">Enviar</button>
       <button className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded">Prosseguir</button> */}
